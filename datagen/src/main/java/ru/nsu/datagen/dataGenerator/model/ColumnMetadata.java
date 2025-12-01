22b6bc2a7d85efc0d70ff8ea7a0a4db28074e4cd
Fedor <f.e.pivovarov@gmail.com>
2025-12-01 22:47:04 +0700

    add array-type flag to ColumnMetadata

--- datagen/src/main/java/ru/nsu/datagen/dataGenerator/model/ColumnMetadata.java
+++ datagen/src/main/java/ru/nsu/datagen/dataGenerator/model/ColumnMetadata.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class ColumnMetadata {
     private final String name;
     private final String dataType;
+    private final String sourceDataType;
     private final boolean isPrimaryKey;
     private final boolean isForeignKey;
     private final boolean isUnique;
@@ -12,22 +13,40 @@ public class ColumnMetadata {
     private final int recordCount;
     private final Integer maxLength;
     private final ForeignKeyMetadata foreignKeyMetadata;
+    private final boolean isArray;
 
     public ColumnMetadata(String name, String dataType, boolean isPrimaryKey,
                           boolean isForeignKey, boolean isUnique, double nullPercentage,
                           int recordCount, Integer maxLength, ForeignKeyMetadata foreignKeyMetadata) {
         this.name = name;
-        this.dataType = dataType;
         this.isPrimaryKey = isPrimaryKey;
+        this.sourceDataType = dataType;
         this.isForeignKey = isForeignKey;
         this.isUnique = isUnique;
         this.nullPercentage = nullPercentage;
         this.recordCount = recordCount;
         this.maxLength = maxLength;
         this.foreignKeyMetadata = foreignKeyMetadata;
+        this.isArray = dataType.contains("[") && !dataType.contains("char");
+
+        if (isArray) {
+            char dataTypeCharArray[] = dataType.toCharArray();
+            StringBuilder dataTypeBuilder = new StringBuilder();
+            for (int i = 0; i < dataTypeCharArray.length; i++) {
+                if (dataTypeCharArray[i] == '[') {
+                    break;
+                }
+                dataTypeBuilder.append(dataTypeCharArray[i]);
+            }
+            this.dataType = dataTypeBuilder.toString();
+        } else {
+            this.dataType = dataType;
+        }
+
     }
 
     public String getName() { return name; }
+    public String getSourceDataType() { return sourceDataType; }
     public String getDataType() { return dataType; }
     public boolean isPrimaryKey() { return isPrimaryKey; }
     public boolean isForeignKey() { return isForeignKey; }
@@ -36,6 +55,7 @@ public class ColumnMetadata {
     public int getRecordCount() {return recordCount; }
     public Integer getMaxLength() { return maxLength; }
     public ForeignKeyMetadata getForeignKeyMetadata() { return foreignKeyMetadata; }
+    public boolean getIsArray() { return isArray; }
 
     @Override
     public boolean equals(Object o) {
