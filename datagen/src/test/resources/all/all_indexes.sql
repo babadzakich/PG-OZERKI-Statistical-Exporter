780195414bc23089d66644b5f0479ce54868537f
dromankin <d.romankin@g.nsu.ru>
2026-05-10 01:29:46 +0700

    Исправлены конфиги для теста all часть 2

--- datagen/src/test/resources/all/all_indexes.sql
+++ datagen/src/test/resources/all/all_indexes.sql
@@ -1,8 +1,5 @@
 -- === INDEXES ===
 
-CREATE INDEX routes_departure_airport_lower_idx ON bookings.routes USING btree (departure_airport, lower(validity));
-
-
 CREATE INDEX segments_flight_id_idx ON bookings.segments USING btree (flight_id);
 
 
